c5fd750e10105270373a285b3b4dec0b6ab85728
Serhii Kyrychenko <skir79@gmail.com>
2021-01-05 14:34:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "a250232-test-instance" {
   ami           = "ami-0dc8d444ee2a42d8a"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-04227aec39b74f2bd"
+  subnet_id     = "subnet-012f41946fd900e6c"
 }
