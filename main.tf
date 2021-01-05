683c4bd86f4b88eae7998339c77d1ff567b5b963
Serhii Kyrychenko <skir79@gmail.com>
2021-01-05 14:32:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "a250232-test_instance" {
+resource "aws_instance" "a250232-test-instance" {
   ami           = "ami-0dc8d444ee2a42d8a"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-012f41946fd900e6c"
+  subnet_id     = "subnet-04227aec39b74f2bd"
 }
