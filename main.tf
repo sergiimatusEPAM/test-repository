cca40ea377b0078d59b3312c17ad48100b7401e6
sergiimatusEPAM <49905012+sergiimatusEPAM@users.noreply.github.com>
2021-01-06 17:11:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_subnet_ids" "main" {
 }
 
 resource "aws_instance" "test_instance" {
-  ami           = "ami-00ddb0e5626798373"
+  ami           = "ami-07d9160fa81ccffb5"
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet_ids.main.ids
+  subnet_id     = element(tolist(data.aws_subnet_ids.main.ids), 0)
 }
