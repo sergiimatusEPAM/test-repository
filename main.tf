c1a8c841d4ab14a56d732a78d5f480e07aaf9eb8
sergiimatusEPAM <49905012+sergiimatusEPAM@users.noreply.github.com>
2021-01-06 18:43:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ data "aws_subnet_ids" "main" {
   }
 }
 
-#resource "aws_instance" "test_instance" {
-#  ami           = "ami-07d9160fa81ccffb5"
-#  instance_type = "t2.micro"
-#  subnet_id     = element(tolist(data.aws_subnet_ids.main.ids), 0)
-#}
+resource "aws_instance" "test_instance" {
+  ami           = "ami-07d9160fa81ccffb5"
+  instance_type = "t2.micro"
+  subnet_id     = element(tolist(data.aws_subnet_ids.main.ids), 0)
+}
