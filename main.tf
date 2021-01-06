0b0b020ddcbdaa8d2824ea543d93091bf7cf98a4
Serhii Kyrychenko <skir79@gmail.com>
2021-01-06 13:11:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1"
+  profile = "default"
 }
 
-resource "aws_instance" "a250232-test-instance" {
-  ami           = "ami-0dc8d444ee2a42d8a"
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["fr-vpc-1"]
+  }
+}
+
+data "aws_subnet_ids" "main" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name   = "tag:Name"
+    values = ["revelation-sdlc-preprod-public-eu-west-1c"]
+  }
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-00ddb0e5626798373"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-021d2bf0185d67882"
+  subnet_id     = data.aws_subnet_ids.main.id
 }
